243ccb6a1083aa88c2b2aacde46f8eab360a82fe
javiezcobar <javiezcobar@gmail.com>
2023-05-09 23:17:41 -0300

    fix some style issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 import Gallery from './component/Gallery';
 import GlobalMenu from './component/GlobalMenu';
 import LocalMenu from './component/LocalMenu';
+import NextMatch from './component/NextMatch'
+import LastMatch from './component/LastMatch'
+import TeamStandings from './component/TeamStanding'
 
 
 
@@ -14,6 +17,21 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <GlobalMenu />
     <LocalMenu />
     <Gallery/>
+    <div className='container-grid'>
+      <div className='matchContainer'>
+        <h2 className='h2Title'>Partidos</h2>
+        <div className='container-flex'>
+        <NextMatch />
+        <LastMatch />
+        </div>
+      </div>
+      <div className='matchContainer-w'>
+        <h2 className='h2Title-b'>Posiciones</h2>
+        <div className='container-flex'>
+        <TeamStandings />
+        </div>
+      </div>
+    </div>
   </React.StrictMode>,
 );
 
