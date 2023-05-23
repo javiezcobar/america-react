c407698af15ad80d8bd95d2bb3095f3592eb347a
javiezcobar <javiezcobar@gmail.com>
2023-05-23 20:59:00 -0300

    mejoras en el codigo

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy:{
       "/api": {
-        target: '/.netlify/functions/proxy',
+        target: 'https://es.besoccer.com/equipo/america-cali',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
